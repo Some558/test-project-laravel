7b8e325603865b218bd636d4df80ea848f2c5f7c
Some558 <167545573+Some558@users.noreply.github.com>
2024-07-31 05:56:18 +0900

    apiKEYもとに戻し

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,8 +9,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        api: __DIR__.'/../routes/api.php',
-        apiPrefix: 'prefix/api',
         health: '/up',
     )
 
